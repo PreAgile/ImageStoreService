9d6c300fdf02cd05a246e0774eb794387c2a7eb5
Meyon Soo(Harvy)  Kim <39788337+PreAgile@users.noreply.github.com>
2024-07-10 13:49:49 +0900

    Conflict

---
+++ src/main/java/com/example/imagestorageservice/UnsplashImageStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.imagestorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsplashImageStorageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsplashImageStorageServiceApplication.class, args);
+	}
+
+}
